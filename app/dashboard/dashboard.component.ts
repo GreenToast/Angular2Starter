2f32d5b3360bf36e2300b0849d39cde3f17cd8d5
GreenToast <dennisneuendorf@gmail.com>
2016-05-16 10:03:16 +0200

    basic-structure-demo setup

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
+import { DashboardSectionComponent } from "./dashboard-section.component";
 
 @Component({
     template: require('./dashboard.component.html'),
+    //directives: [DashboardSectionComponent]
 })
 export class DashboardComponent {
+    /*second:string = "Second Section";
+    
+    print(text:string){
+        console.log('clicked '+text);
+    }*/
 }
--- test/e2e/e2etest-tsd.d.ts
+++
@@ -1,2 +0,0 @@
-///<reference path="../../node_modules/angular2/ts/typings/jasmine/jasmine.d.ts"/>
-///<reference path="../../node_modules/angular2/ts/typings/angular-protractor/angular-protractor.d.ts"/>
